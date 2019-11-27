50aed42c0d366d27deb6d6feb8afb9ed7df2edbc
Steve Hill <15816322+MrStevenHill@users.noreply.github.com>
2019-11-27 08:00:11 +0000

    Add title

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 
 import Header from "./components/Header";
 import Logo from "./components/Logo";
-import PopupLink from "./components/PopupLink";
 import Counter from "./components/Counter";
 
 const Centered = styled.div`
@@ -14,10 +13,7 @@ const App = () => (
   <Centered>
     <Header>
       <Logo />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <PopupLink url="https://reactjs.org" text="Learn React" />
+      <h1>Redux Counter</h1>
       <Counter />
     </Header>
   </Centered>
